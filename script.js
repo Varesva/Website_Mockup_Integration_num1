d7b729812a52885c0cf2abb6bc91585b42eb2450
Varesva <varesva.dev@gmail.com>
2022-08-14 00:04:18 +0200

    near everything ok, doublecheck needed

--- script.js
+++ script.js
@@ -4,14 +4,14 @@ let yearCopyright = dateCopyright.getFullYear();
 document.getElementById("year").innerHTML = yearCopyright;
 
 // SHOW MENU ON SCROLL
-const menuHeader = document.getElementById('header');
+const mediaQuery = window.matchMedia("(max-width: 768px)");
+const menuHeader = document.getElementById("header");
 
 // declare + initialize
 let lastScroll = window.scrollY;
 // console.log(menuHeader);
-
 window.addEventListener("scroll", function () {
-    if (lastScroll < this.window.scrollY) {
+    if (lastScroll < this.window.scrollY && mediaQuery.matches) {
         menuHeader.classList.add("hide-header");
     } else {
         menuHeader.classList.remove("hide-header");
